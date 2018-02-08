6c4cea7ed83a1fac3d98bb0bfaa9ecf6179990c2
JesseT1 <taylorjt@g.cofc.edu>
2018-02-08 16:03:31 -0500

    Version 2

--- app/src/main/java/com/example/jesse/tipcalculator_jesse/MainActivity.java
+++ app/src/main/java/com/example/jesse/tipcalculator_jesse/MainActivity.java
@@ -2,58 +2,122 @@ package com.example.jesse.tipcalculator_jesse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+
+    }
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+
+    }
+    */
+    boolean values;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void calculate(View view){
+    public void main(View view){
         EditText input = findViewById(R.id.TotalAmount);
         String inputBill = input.getText().toString();
         EditText inputPeople = findViewById(R.id.PeopleAmount);
         String people = inputPeople.getText().toString();
-        float peopleN = Float.valueOf(people);
-        EditText customInput = findViewById(R.id.CustomTip);
-        String customTip = customInput.getText().toString();
-        float custom = Float.valueOf(customTip);
-        float bill = Float.valueOf(inputBill);
-        float totalBill = 0;
-        RadioButton twelve = findViewById(R.id.Twelve);
-        RadioButton fifteen = findViewById(R.id.Fifteen);
-        RadioButton eighteen = findViewById(R.id.Eighteen);
-        RadioButton customButton = findViewById(R.id.Other);
-
-
-        float tipAmount;
-
-        if(twelve.isChecked()){
-            tipAmount = (bill * 12 / 100);
-            totalBill = (tipAmount + bill) / peopleN;
+        if(TextUtils.isEmpty(inputBill)){
+            input.setError("You did not input a value for the bill!");
+            values = false;
+            return;
         }
-        else if(fifteen.isChecked()){
-            tipAmount = (bill * 15 / 100);
-            totalBill = (tipAmount + bill) / peopleN;
+        if(TextUtils.isEmpty(people)){
+            inputPeople.setError("You did not input a number of people!");
+            values = false;
+            return;
         }
-        else if(eighteen.isChecked()){
-            tipAmount = (bill * 18 / 100);
-            totalBill = (tipAmount + bill) / peopleN;
+        else{
+            values = true;
         }
-        else if(customButton.isChecked()){
-            tipAmount = (bill * custom / 100);
-            totalBill = (tipAmount + bill) / peopleN;
+        if(values == true){
+            calculate();
         }
-
-        TextView total = findViewById(R.id.Total);
-        total.setText(String.format("Your total will be $%.2f.", totalBill));
     }
+    public void calculate(){
+            EditText input = findViewById(R.id.TotalAmount);
+            String inputBill = input.getText().toString();
+            EditText inputPeople = findViewById(R.id.PeopleAmount);
+            String people = inputPeople.getText().toString();
+            EditText customInput = findViewById(R.id.CustomTip);
+            String customTip = customInput.getText().toString();
+            float custom = Float.valueOf(customTip);
+            float peopleN = Float.valueOf(people);
+            float bill = Float.valueOf(inputBill);
+            float totalBill = 0;
+            RadioButton twelve = findViewById(R.id.Twelve);
+            RadioButton fifteen = findViewById(R.id.Fifteen);
+            RadioButton eighteen = findViewById(R.id.Eighteen);
+            RadioButton customButton = findViewById(R.id.Other);
+            float tipAmount;
+
+            if(twelve.isChecked()){
+                twelve.setError(null);
+                fifteen.setError(null);
+                eighteen.setError(null);
+                customButton.setError(null);
+                tipAmount = (bill * 12 / 100);
+                totalBill = (tipAmount + bill) / peopleN;
+            }
+            else if(fifteen.isChecked()){
+                twelve.setError(null);
+                fifteen.setError(null);
+                eighteen.setError(null);
+                customButton.setError(null);
+                tipAmount = (bill * 15 / 100);
+                totalBill = (tipAmount + bill) / peopleN;
+            }
+            else if(eighteen.isChecked()){
+                twelve.setError(null);
+                fifteen.setError(null);
+                eighteen.setError(null);
+                customButton.setError(null);
+                tipAmount = (bill * 18 / 100);
+                totalBill = (tipAmount + bill) / peopleN;
+            }
+            else if(customButton.isChecked()){
+                twelve.setError(null);
+                fifteen.setError(null);
+                eighteen.setError(null);
+                customButton.setError(null);
+                if(customInput == null){
+                    customButton.setError("Set a value here.");
+                }
+                if(custom < 12){
+                    customButton.setError("Please tip an amount of 12% or more.");
+                    return;
+                }
+                tipAmount = (bill * custom / 100);
+                totalBill = (tipAmount + bill) / peopleN;
+            }
+            else{
+                twelve.setError("Please select an amount to tip");
+                fifteen.setError("Please select an amount to tip");
+                eighteen.setError("Please select an amount to tip");
+                customButton.setError("Please select an amount to tip");
+                return;
+            }
+            TextView total = findViewById(R.id.Total);
+            TextView billTotal = findViewById(R.id.billTotal);
+            billTotal.setText(String.format("The total of the bill is $%.2f.", totalBill));
+            total.setText(String.format("The amount for you to tip will be $%.2f.", tipAmount / 10));
+        }
 }
